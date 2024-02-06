7270b9cfbef531e9605cdcf4123a98c01107d61d
Felipe Alencar <felipealencardev@gmail.com>
2024-02-05 22:08:59 -0300

    respond with 200

--- app/main.py
+++ app/main.py
@@ -2,8 +2,21 @@ import socket
 
 
 def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    HOST = "localhost"
+    PORT = 4221
+    SERVER_ADDRESS = (HOST, PORT)
+    HTTP_STATUS_OK_RESPONSE = b"HTTP/1.1 200 OK\r\n\r\n"
+    BUFFER_SIZE = 1024
+
+    server_socket = socket.create_server(SERVER_ADDRESS, reuse_port=True)
+    conn, addr = server_socket.accept()
+
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(BUFFER_SIZE)
+            if not data: break
+            conn.sendall(HTTP_STATUS_OK_RESPONSE)
 
 
 if __name__ == "__main__":
