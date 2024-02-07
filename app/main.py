e33db8b62312d1d6f19de589abf074762678e703
Felipe Alencar <felipealencardev@gmail.com>
2024-02-06 21:36:54 -0300

    Respond with 404

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ def main():
     PORT = 4221
     SERVER_ADDRESS = (HOST, PORT)
     HTTP_STATUS_OK_RESPONSE = b"HTTP/1.1 200 OK\r\n\r\n"
+    HTTP_STATUS_NOT_FOUND_RESPONSE = b"HTTP/1.1 404 Not Found\r\n\r\n"
     BUFFER_SIZE = 1024
 
     server_socket = socket.create_server(SERVER_ADDRESS, reuse_port=True)
@@ -16,7 +17,15 @@ def main():
         while True:
             data = conn.recv(BUFFER_SIZE)
             if not data: break
-            conn.sendall(HTTP_STATUS_OK_RESPONSE)
+            
+            request_list = data.split(b"\r\n")
+            start_line = request_list[0].split()
+            path = start_line[1]
+            
+            if path == b"/":
+                conn.sendall(HTTP_STATUS_OK_RESPONSE)
+            else:
+                conn.sendall(HTTP_STATUS_NOT_FOUND_RESPONSE)
 
 
 if __name__ == "__main__":
